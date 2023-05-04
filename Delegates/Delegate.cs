f095118918b4bb82a797c06c97d73e9ebe32ac51
Siega <Siega@MSI>
2023-05-04 18:27:43 +0700

    Добавьте файлы проекта.

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Delegate
+    {
+        delegate double Operation(double x);
+        Operation[] operations = { Add, Multiply, Divide, Substract, Cube, Sqrt };
+        public void Print(double x)
+        {
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine(operations[i](x));
+            }
+        }
+        static double Add(double x) => x + x;
+        static double Multiply(double x) => Math.Round(x * x, 2);
+        static double Substract(double x) => x - x;
+        static double Divide(double x) => x / x;
+        static double Cube(double x) => Math.Round(x * x * x, 2);
+        static double Sqrt(double x) => Math.Round(Math.Sqrt(x), 2);
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Delegate delegates = new Delegate();
+            Console.WriteLine("Enter a number with floating point");
+            delegates.Print(double.Parse(Console.ReadLine()));
+
+        }
+    }
+}
